aad41aa56978251dc99014bb5470a48b041afc9d
Jannic Holzer <jannic.holzer@gmail.com>
2022-08-20 23:44:16 +0100

    Add ETF and Measurement models

--- etf_track/etfs/models.py
+++ etf_track/etfs/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class ETF(models.Model):
+    exchange = models.CharField(max_length=10)
+    ticker = models.CharField(max_length=10)
+    name = models.CharField(max_length=255)
+    
+
+class Measurement(models.Model):
+    etf_id = models.PositiveIntegerField()
+    date_time = models.DateTimeField()
