82b29a7eb5fc74b4782f571171d767fc55b6ef25
Jannic Holzer <jannic.holzer@gmail.com>
2022-08-21 22:03:48 +0100

    Complete first version of ETF and Measurement classes

--- etf_track/etfs/models.py
+++ etf_track/etfs/models.py
@@ -5,8 +5,12 @@ class ETF(models.Model):
     exchange = models.CharField(max_length=10)
     ticker = models.CharField(max_length=10)
     name = models.CharField(max_length=255)
-    
+    sector = models.CharField(max_length=255)
+
 
 class Measurement(models.Model):
     etf_id = models.PositiveIntegerField()
     date_time = models.DateTimeField()
+    p_e = models.FloatField()
+    ev_ebidta = models.FloatField()
+    expense_ratio = models.FloatField()    
