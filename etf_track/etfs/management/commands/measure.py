c1ed74bccacdd96a2a17f0b4919ac157e0477219
Jannic Holzer <jannic.holzer@gmail.com>
2022-08-22 21:14:37 +0100

    Add management.py interface for making measurement

---
+++ etf_track/etfs/management/commands/measure.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand, CommandError
+
+
+class Command(BaseCommand):
+    help = 'Closes the specified poll for voting'
+
+    def add_arguments(self, parser):
+        parser.add_argument('identifiers', nargs='+', type=int)
