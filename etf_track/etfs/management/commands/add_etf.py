5a3c15d39f6d2ef648ada4207df9351fa94a9cd0
Jannic Holzer <jannic.holzer@gmail.com>
2022-08-30 00:05:12 +0100

    Add logic for creating new row in etf table

--- etf_track/etfs/management/commands/add_etf.py
+++ etf_track/etfs/management/commands/add_etf.py
@@ -1,11 +1,31 @@
 from django.core.management.base import BaseCommand, CommandError
+from etfs.models import ETF
+
+
+ARG_TYPES = {
+    "exchange": str,
+    "ticker": str,
+    "name": str
+}
 
 
 class Command(BaseCommand):
     help = "Add an ETF to the database"
 
     def add_arguments(self, parser):
-        parser.add_argument("identifiers", nargs="+", type=str)
+        for arg in ARG_TYPES:
+            parser.add_argument("--" + arg, required=True, type=ARG_TYPES[arg])
 
     def handle(self, *args, **options):
-        pass
+        identifiers = {}
+        for arg in ARG_TYPES:
+            identifiers[arg] = options[arg]
+        self._save_etf(identifiers)
+        
+    def _save_etf(self, identifiers):
+        etf = ETF(
+            name=identifiers["name"],
+            ticker=identifiers["ticker"],
+            exchange=identifiers["exchange"]
+        )
+        etf.save()
