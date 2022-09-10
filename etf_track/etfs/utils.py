dc140c51dbf43b2ace65a5d48e1dc0cff2b3c56d
Jannic Holzer <jannic.holzer@gmail.com>
2022-09-10 22:38:14 +0100

    Refactor docstring of get_current_datetime

--- etf_track/etfs/utils.py
+++ etf_track/etfs/utils.py
@@ -2,10 +2,12 @@ from datetime import datetime
 
 
 def get_current_datetime(self) -> datetime:
-        """
+    """
+    A utility function for returning the current date and time in a
+    datetime object
+    
+    Returns:
+        A datetime object containing the date / time of measurement.
 
-        Returns:
-            A datetime object containing the date / time of measurement.
-
-        """
-        return datetime.now()
+    """
+    return datetime.now()
