0fd0e54244312754fe45881960f6246075021bf4
Jannic Holzer <jannic.holzer@gmail.com>
2022-09-10 22:15:54 +0100

    Add utils.py

---
+++ etf_track/etfs/utils.py
@@ -0,0 +1,11 @@
+from datetime import datetime
+
+
+def get_current_datetime(self) -> datetime:
+        """
+
+        Returns:
+            A datetime object containing the date / time of measurement.
+
+        """
+        return datetime.now()
