0e99c91709ec3b00d74ef56fa9acf62e0c842a2e
Jannic Holzer <jannic.holzer@gmail.com>
2022-09-19 00:38:42 +0100

    Refactor read to read_all

---
+++ etf_track/etfs/management/commands/read_all.py
@@ -0,0 +1,20 @@
+from django.core.management.base import BaseCommand, CommandError
+from etfs.models import ETF
+
+
+class Command(BaseCommand):
+    help = "Run a measurement for the specified ETF"
+
+    def add_arguments(self, parser):
+        parser.add_argument("--" + "etf_issuer", required=True, type=str)
+
+    def handle(self, *args, **options):
+        etf_issuer = options["etf_issuer"]
+        self._validate_etf(etf_issuer)
+
+    def _validate_etf(self, etf_issuer: str) -> None:
+        """Check if the name of an ETF issuer is valid and raise an
+        exception if not
+        """
+        if not ETF.objects.filter(etf_issuer=etf_issuer).exists():
+            raise CommandError("ETF not found in database")
