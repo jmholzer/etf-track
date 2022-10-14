e3507f6df0ed578a1714b3c892136c352910b188
Jannic Holzer <jannic.holzer@gmail.com>
2022-10-14 03:15:25 +0100

    Rescope ETFReader to download and clean holdings

--- etf_track/etfs/reader.py
+++ etf_track/etfs/reader.py
@@ -9,31 +9,31 @@ from pandas import read_csv, DataFrame
 
 class ETFReader(ABC):
     """The base class for all ETF readers. Reading an ETF involves:
-        1. Downloading holding data
-        2. Calculating stats from holding data
-        3. Entering stats into db
+        1. Downloading the holding data
+        2. Cleaning the data
 
     Attributes:
+        _identifiers
 
     Methods:
         _download
-        _parse_average_price_earnings
-        _parse_average_ev_ebitda
     """
 
     def __init__(self, identifiers: Dict[str, str]):
         """Initialise an ETFReader object"""
         self._identifiers = identifiers
 
-    def read(self) -> None:
-        """The endpoint for reading an ETF and inserting the reading
-        into the application database
+    def read(self) -> DataFrame:
+        """The endpoint for reading an ETF's holdings and returning
+        them in a ``DataFrame``
+
+        Returns:
+            A ``DataFrame`` containing the ticker, exchange and weight of
+            the holdings of the ETF
         """
-        self._download()
-        self._clean_holdings()
-        self._parse_average_price_earnings()
-        self._parse_average_ev_ebitda()
-        self._insert_reading()
+        holdings = self._download()
+        self._clean_holdings(holdings)
+        return holdings
 
     @abstractmethod
     def _download(self) -> DataFrame:
@@ -46,36 +46,10 @@ class ETFReader(ABC):
         pass
 
     @abstractmethod
-    def _clean_holdings(self) -> None:
+    def _clean_holdings(self, holdings: DataFrame) -> None:
         """Apply data-cleaning steps to the holdings ``DataFrame``, applying
         the chanes in place
         """
-        pass
-
-    @abstractmethod
-    def _parse_average_price_earnings(self) -> float:
-        """Parse the data contained in the holdings DataFrame to calculate
-        the average price / earnings ratio for the ETF.
-
-        Returns:
-            The average price / earnings ratio for the ETF as float.
-
-        """
-        pass
-
-    @abstractmethod
-    def _parse_average_ev_ebitda(self) -> float:
-        """Parse the data contained in the holdings DataFrame to calculate
-        the average EV / EBITDA ratio for the ETF.
-
-        Returns:
-            The average EV / EBITDA ratio for the ETF as float.
-        """
-        pass
-
-    @abstractmethod
-    def _insert_reading(self) -> None:
-        """Insert a reading into the application database"""
         pass
 
 
@@ -100,37 +74,14 @@ class iSharesETFReader(ETFReader):
         Returns:
             A ``DataFrame`` containing holdings data for ETF being processed.
         """
-        self._holdings = read_csv(self._identifiers["holdings_url"], skiprows=[0, 1])
+        return read_csv(self._identifiers["holdings_url"], skiprows=[0, 1])
 
-    def _clean_holdings(self) -> None:
+    def _clean_holdings(self, holdings) -> None:
         """Apply data-cleaning steps to the holdings ``DataFrame``, applying
         changes in place
         """
-        self._holdings = self._holdings[self._holdings.Sector.notnull()]
-        self._holdings = self._holdings[self._holdings.Sector != "Cash and/or Derivatives"]
-
-    def _parse_average_price_earnings(self) -> float:
-        """Parse the data contained in the holdings DataFrame to calculate
-        the average price / earnings ratio for the ETF.
-
-        Returns:
-            The average price / earnings ratio for the ETF as float.
-        """
-        pass
-
-    def _parse_average_ev_ebitda(self) -> float:
-        """Parse the data contained in the holdings DataFrame to calculate
-        the average EV / EBITDA ratio for the ETF.
-
-        Returns:
-            The average EV / EBITDA ratio for the ETF as float.
-
-        """
-        pass
-
-    def _insert_reading(self) -> None:
-        """Insert a reading into the application database"""
-        print(self._holdings)
+        holdings = holdings[holdings.Sector.notnull()]
+        holdings = holdings[holdings.Sector != "Cash and/or Derivatives"]
 
 
 class ETFReaderCreator(ABC):
