af356545dd53cf3296b567f8f69f5f8196d13de4
Jannic Holzer <jannic.holzer@gmail.com>
2022-10-23 06:43:04 +0100

    Add test for _find_orphan_tickers

--- etf_track/etfs/tests.py
+++ etf_track/etfs/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from pandas import DataFrame
+
+from etfs.reader import _find_orphan_tickers
+
 
 # Create your tests here.
+class TestUpdateHoldings(TestCase):
+    def setUp(self):
+        self._downloaded_holdings = DataFrame.from_dict({
+            "ticker": ["AAPL"],
+            "percentage": [13.1],
+        })
+        self._stored_holdings = DataFrame.from_dict({
+            "ticker": ["AAPL", "TSLA"],
+            "percentage": [13.1, 10.2]
+        })
+
+    def test_find_orphan_tickers(self):
+        result = _find_orphan_tickers(self._downloaded_holdings, self._stored_holdings)
+        expected = {"TSLA"}
+        self.assertEqual(result, expected)
