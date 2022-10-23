3e0cca37f1f26914301a3a0317534bd66d9efec5
Jannic Holzer <jannic.holzer@gmail.com>
2022-10-23 07:01:27 +0100

    Add test for _query_holdings_by_etf_id

--- etf_track/etfs/tests.py
+++ etf_track/etfs/tests.py
@@ -1,22 +1,33 @@
 from django.test import TestCase
 from pandas import DataFrame
 
-from etfs.reader import _find_orphan_tickers
+from etfs.models import Holdings
+from etfs.reader import (
+    _find_orphan_tickers,
+    _query_holdings_by_etf_id,
+)
 
 
-# Create your tests here.
 class TestUpdateHoldings(TestCase):
     def setUp(self):
-        self._downloaded_holdings = DataFrame.from_dict({
-            "ticker": ["AAPL"],
-            "percentage": [13.1],
-        })
-        self._stored_holdings = DataFrame.from_dict({
-            "ticker": ["AAPL", "TSLA"],
-            "percentage": [13.1, 10.2]
-        })
+        Holdings.objects.create(etf_id=1, ticker="AAPL", percentage=13.1)
+        Holdings.objects.create(etf_id=1, ticker="TSLA", percentage=10.1)
+        Holdings.objects.create(etf_id=2, ticker="NVDA", percentage=11.2)
 
     def test_find_orphan_tickers(self):
-        result = _find_orphan_tickers(self._downloaded_holdings, self._stored_holdings)
+        downloaded_holdings = DataFrame.from_dict(
+            {"ticker": ["AAPL"], "percentage": [13.1]}
+        )
+        stored_holdings = DataFrame.from_dict(
+            {"ticker": ["AAPL", "TSLA"], "percentage": [13.1, 10.2]}
+        )
+        result = _find_orphan_tickers(downloaded_holdings, stored_holdings)
         expected = {"TSLA"}
         self.assertEqual(result, expected)
+
+    def test_query_holdings_by_etf_id(self):
+        expected = DataFrame.from_dict(
+            {"ticker": ["AAPL", "TSLA"], "percentage": [13.1, 10.1]}
+        )
+        result = _query_holdings_by_etf_id(1)
+        self.assertTrue(result.equals(expected))
