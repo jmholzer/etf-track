c75a70080bf8f86db850736d71ce58953eb028fb
Jannic Holzer <jannic.holzer@gmail.com>
2022-10-31 01:31:56 +0000

    Add fundamentals.loader, fundamentals.reader

---
+++ etf_track/etfs/fundamentals/loader.py
@@ -0,0 +1,47 @@
+from etfs.models import Fundamentals
+from etfs.models import Holdings
+
+from typing import Set
+
+
+def update_fundamentals() -> None:
+    """Update the fundamentals table by comparing tickers with
+    the Holdings table.
+    """
+    pass
+
+
+def _read_all_holding_tickers() -> Set[str]:
+    """Read all the tickers in the Holdings table
+
+    Returns:
+        A set of all the tickers in the Holdings table
+    """
+    pass
+
+
+def _read_all_fundamentals_tickers() -> Set[str]:
+    """Read all the tickers in the Holdings table
+
+    Returns:
+        A set of all the tickers in the Holdings table
+    """
+    pass
+
+
+def _remove_unused_tickers(unused_tickers: Set[str]) -> None:
+    """Remove unused tickers from the Fundamentals table.
+
+    Arguments:
+        unused_tickers: a set containing the unused tickers to remove
+    """
+    pass
+
+
+def _add_missing_tickers(missing_tickers: Set[str]) -> None:
+    """Add missing tickers to the Fundamentals table.
+
+    Arguments:
+        missing_tickers: a set containing the missing tickers to add
+    """
+    pass
